b990fe3d8ed1489e6849ff9e974dcb8d3d3da4b7
Sarthak-G0yal <sarthakgoyal487@gmail.com>
2024-07-06 14:10:47 +0530

    Add Setting And Ship Module

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,15 +2,21 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """Overall Class to Manage game assests and behaviour"""
 
     def __init__(self):
         # Initialise the game, and create game resources.
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
     def run_game(self):
         # Start the main loop for the game
@@ -21,9 +27,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # make the most recent drawn screen visible
-
             pygame.display.flip()
 
 if __name__ == '__main__':
