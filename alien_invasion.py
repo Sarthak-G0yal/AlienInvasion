948b1514a4beef719e30469781ec7db3e915b2a7
Sarthak Goyal <sarthakgoyal487@gmail.com>
2024-07-08 15:29:55 +0530

    Add Bullet and Related Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall Class to Manage game assests and behaviour"""
@@ -19,6 +20,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def _check_events(self):
         # Watch for keybord and mouse events
@@ -41,6 +43,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_event(self,event):
         #Respond to key presses.
@@ -50,21 +54,41 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             #   Stop the ship moving to left.
             self.ship.moving_left = False
+    
+    def _fire_bullet(self):
+        #create a new bullet and add it to the bullet group.
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self) 
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
          # Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
 
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
+
             # make the most recent drawn screen visible
             pygame.display.flip()
+    
+    def _update_bullets(self):
+                #Updates position of bullets and get rid of old Bullets.
+
+                self.bullets.update()
+
+                #Get rid of bullets that have disappeared.
+                for bullet in self.bullets.copy():
+                    if bullet.rect.bottom <= 0:
+                        self.bullets.remove(bullet)
 
     def run_game(self):
         # Start the main loop for the game
 
         while True:
-            self._check_events()
+            self._check_events() 
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
 
 if __name__ == '__main__':
